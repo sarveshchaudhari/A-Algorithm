b2a33bd7ee6cdf30c4341e5f10538cc91b1ceabb
sarveshchaudhari <chaudharisarvesh10@gmail.com>
2024-08-22 11:01:41 +0530

    first Commit

---
+++ A_Star.py
@@ -0,0 +1,9 @@
+import pygame
+import math
+from queue import PriorityQueue
+
+WIDTH = 800
+#display window size
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("A* Path Finding Algorithm")
+
